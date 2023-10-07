183939feba3107d01972a3fbfb834328bbbb5be3
Otaviopeixoto1 <otaviopeixotogm@gmail.com>
2023-10-07 14:15:13 -0300

    added documentation, a viewport resizing callback and simple input handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main()
 {
+
+    // GLFW: initialize and configure
+    // ------------------------------
     GLFWwindow* window;
 
     if (!glfwInit())
@@ -11,10 +18,24 @@ int main()
         return -1;
     } 
 
+    // makes sure the glfw is the right version and using core profile:
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+
+    // GLFW: rendering window creation
+    // -------------------------------
     window = glfwCreateWindow(640,360,"testWindow",NULL,NULL);
     glfwMakeContextCurrent(window);
 
 
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "couldn't load openGL" << std::endl;
@@ -23,18 +44,84 @@ int main()
         return -1;
     }
 
+
+    // GLFW: initial viewport configuration and callbacks
+    // --------------------------------------------------
+
+
+    // the viewport is the rendering window i.e. its the region where openGL will draw and it can be different from
+    // GLFW's window size processed coordinates in OpenGL are between -1 and 1 so we effectively map from the range 
+    // (-1 to 1) to (0, 640) and (0, 360). 
+    glViewport(0, 0, 640, 360);
+
+
+    // callback functions can be customized for certain events:
+
+    // this callback is used after a resize happens to the GLFW window and we can pass a custom function that follows
+    // a defined template:
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+    // We register the callback functions after we've created the window and before the render loop is initiated. 
+
+
+    //we call glClear and clear the color buffer, the entire color buffer will be filled with the color as configured 
+    //by glClearColor:
     glClearColor(0.25f, 0.5f,0.75f,1.0f);
 
+
+    // Render Loop
+    // -----------
     while(!glfwWindowShouldClose(window))
     {
-        glfwPollEvents();
+        // Input Handling
+        // --------------
+        processInput(window);
+        
 
+        // Rendering
+        // ---------
         glClear(GL_COLOR_BUFFER_BIT);
 
+
+
+        // GLFW: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+
+        // Double buffering: When an application draws in a single buffer the resulting image may have flickering.
+        // scince the resulting output image is not drawn in an instant, but drawn pixel by pixel. To circumvent these
+        // issues, windowing applications apply a double buffer for rendering. The front buffer contains the final 
+        // output image that is shown at the screen, while all the rendering commands draw to the back buffer. As soon
+        // as all the rendering commands are finished we swap the back buffer to the front buffer:
         glfwSwapBuffers(window);
+
+
+        // PollEvents: checks if any events are triggered (like keyboard input or mouse movement events), updates the 
+        // window state and calls the corresponding functions (which we can register via callback methods):
+        glfwPollEvents();
     }
 
-    glfwTerminate();
 
+    // GLFW: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
     return 0;
+}
+
+
+// A callback that is used after a resize happens to the GLFW window. This is passed
+// to glfwSetFramebufferSizeCallback() and can be customized to use the arguments that are automatically
+// filled in by GLFW
+// -----------------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
+
+// Process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    //if the user pressed escape (Esc), then the window closes
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
