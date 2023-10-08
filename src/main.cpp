01e4eb8d0b4ffa5ff1fc975fcc88948c2d54ebbe
Otaviopeixoto1 <otaviopeixotogm@gmail.com>
2023-10-08 10:21:15 -0300

    added a simple test of drawing a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Temporary hardcoded shader programs 
+// -------------------------
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+///////////////////////////////////////////////////////////////
+
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -69,6 +86,141 @@ int main()
     glClearColor(0.25f, 0.5f,0.75f,1.0f);
 
 
+
+
+
+
+
+
+
+
+
+
+    // Build and compile the shader programs
+    // -------------------------------------
+
+    // 1) Vertex shader
+    // ----------------
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // add the source code (hardcoded string or read from file)
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // checking for compile-time errors:
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // 2) Fragment shader
+    // ------------------
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // checking for compile-time errors:
+    //int  success;
+    //char infoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    // Linking the final program
+    // -------------------------
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    // attach all previous shaders to the same program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+
+    // checking for linking errors:
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    // all shaders have to be deleted once linked to their main program:
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+
+
+
+    // Setup vertex data and buffers
+    // -----------------------------
+
+    // the data for a simple triangle:
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };  
+    
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // a vertex array object can be created for easily managing the vertex attribute configuration (which stores the
+    // information required to intepret the vertex buffers) and which buffer is associated with that config
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO); 
+    // after binding, the vertex attributes configured from now on will be associated with this specific VAO
+    glBindVertexArray(VAO); 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+    
+    // a buffer object to store the triangle data:
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);  //args = (number of buffers generated, buffer id/name references)
+    // bind the buffer to an array buffer (the type used for the vertex shader data):
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+
+    // copy the previously defined "vertices" data into our array buffer in static draw mode. the draw modes are:
+    // GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+    // GL_STATIC_DRAW: the data is set only once and used many times.
+    // GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+
+    // after the buffer was set, we must inform how to interpret the vertex data stored in it:
+
+    // here, the vertex attribute 0 contained in the buffer is a vec3 (3D vector):
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);  
+    // the vertex shader program can now take the vertex attribute 0 as an input after it is enabled:
+    glEnableVertexAttribArray(0);  
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // since the VAO already contains the reference to VBO after the call to glVertexAttribPointer(), the
+    // VBO can be safely unbound:
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    // the same can be done for the VAO, as long as it is bound again when we try to draw the object
+    glBindVertexArray(0);
+
+    // *unbinding is not necessary since the process of modifying a new VAO already calls glBindVertexArray()
+    // and that would unbind the existing VAO
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
     // Render Loop
     // -----------
     while(!glfwWindowShouldClose(window))
@@ -82,6 +234,12 @@ int main()
         // ---------
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // use the shader program to render:
+        glUseProgram(shaderProgram);
+        // set the vertex data and its configuration that will be used for drawing
+        glBindVertexArray(VAO);
+        // draw the data using the currently active shader
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
         // GLFW: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
